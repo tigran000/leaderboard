5ef4f819c97dfb535938a8b26d2e606339ad6724
karabala <tigranharutyunyan59@gmail.com>
2019-11-17 12:58:49 +0400

    adding Dialog

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
 
-function App() {
+import FormDialog from "./FormDialog";
+let userId = 2;
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 650
+  }
+});
+
+const initialRows = [
+  { id: 0, firstName: "Geary", lastName: "Alice", score: 96 },
+  { id: 1, firstName: "Junge", lastName: "John", score: 96 },
+  { id: 2, firstName: "Vera", lastName: "Rob", score: 88 }
+];
+
+export default function SimpleTable() {
+  const classes = useStyles();
+
+  const [rows, setRows] = useState(initialRows);
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const handleDelete = id =>
+    setRows(prevRows => prevRows.filter(row => row.id !== id));
+
+  const handleAddEdit = (user, firstName, lastName, score) => {
+    if (user) {
+      const updatedRows = rows.map(row => {
+        if (row.id === user.id) {
+          return { id: user.id, firstName, lastName, score };
+        }
+        return row;
+      });
+
+      setRows(updatedRows);
+    } else {
+      setRows([...rows, { id: ++userId, firstName, lastName, score }]);
+    }
+
+    handleClose();
+  };
+  const handleClickOpen = row => {
+    setUser(row);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => handleClickOpen()}
+      >
+        ADD
+      </Button>
+      <Paper className={classes.root}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" style={{ width: "40%" }}>
+                Name
+              </TableCell>
+              <TableCell align="center" style={{ width: "30%" }}>
+                Score
+              </TableCell>
+              <TableCell align="center" style={{ width: "30%" }}>
+                Action
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell align="center" style={{ width: "40%" }}>
+                  {row.firstName} {row.lastName}
+                </TableCell>
+                <TableCell align="center" style={{ width: "40%" }}>
+                  {row.score}
+                </TableCell>
+                <TableCell align="center" style={{ width: "30%" }}>
+                  <IconButton onClick={() => handleClickOpen(row)}>
+                    <EditIcon color="primary" />
+                  </IconButton>
+                  <IconButton onClick={() => handleDelete(row.id)}>
+                    <DeleteIcon color="secondary" />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <FormDialog
+          handleClose={handleClose}
+          open={open}
+          user={user}
+          handleAddEdit={handleAddEdit}
+        />
+      </Paper>
     </div>
   );
 }
-
-export default App;
---
+++ src/FormDialog.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Slider from "@material-ui/core/Slider";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import Typography from "@material-ui/core/Typography";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function FormDialog({ handleClose, open, user, handleAddEdit }) {
+  const [score, setScore] = useState(50);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const handleScoreChange = (_, newValue) => setScore(newValue);
+  const handleFirstNameChange = event => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameChange = event => {
+    setLastName(event.target.value);
+  };
+  const setValues = () => {
+    if (user) {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setScore(user.score);
+    } else {
+      setFirstName("");
+      setLastName("");
+      setScore(50);
+    }
+  };
+
+  useEffect(() => {
+    setValues();
+  }, [setValues]);
+
+  return (
+    <Dialog
+      open={open}
+      onClose={() => {
+        handleClose();
+        setValues();
+      }}
+      aria-labelledby="form-dialog-title"
+    >
+      <DialogTitle>{user ? "Edit" : "Add"}</DialogTitle>
+      <Box
+        component="form"
+        onSubmit={e => {
+          e.preventDefault();
+          handleAddEdit(user, firstName, lastName, score);
+        }}
+      >
+        <DialogContent dividers={true}>
+          <TextField
+            value={firstName}
+            onChange={handleFirstNameChange}
+            margin="dense"
+            label="First Name"
+            required
+            fullWidth
+          />
+          <TextField
+            value={lastName}
+            onChange={handleLastNameChange}
+            margin="dense"
+            required
+            label="Last Name"
+            fullWidth
+          />
+          <Box style={{ marginTop: "10px" }}>
+            <Typography gutterBottom>Score</Typography>
+            <Slider
+              onChange={handleScoreChange}
+              value={score}
+              valueLabelDisplay="auto"
+              step={1}
+              min={0}
+              max={100}
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              handleClose();
+              setValues();
+            }}
+            color="primary"
+          >
+            Cancel
+          </Button>
+          <Button color="primary" type="submit">
+            {user ? "Edit" : "Add"}
+          </Button>
+        </DialogActions>
+      </Box>
+    </Dialog>
+  );
+}
